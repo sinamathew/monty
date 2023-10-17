8f5771a3be8329ed2fb7f6b66ed44eccf643f669
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-17 18:50:22 +0100

    Up to you, Elmi

--- main.c
+++ main.c
@@ -5,20 +5,25 @@
  * @ac: argument counter
  * @av: argumnet vector
  *
- * Return: 0 on success exit, else error
+ * Return: 0 on success, exit with exit status EXIT_FAILURE on error
  *
  * Description: Monty Byte Code
  *
  * By: Sina & Elmi
  */
-int main(int *ac, char **av)
+int main(int ac, char **av)
 {
+
 	if (ac != 2)
 	{
-		printf("USAGE: monty file\n);
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
+	else
+	{
+		printf("All good\n");
+	}
 
 	return (0);
 }
