13698c51d7a342089c2886ed49ceec588d332bfd
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-17 18:39:22 +0100

    Monty

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * main - the monty main code
+ * @ac: argument counter
+ * @av: argumnet vector
+ *
+ * Return: 0 on success exit, else error
+ *
+ * Description: Monty Byte Code
+ *
+ * By: Sina & Elmi
+ */
+int main(int *ac, char **av)
+{
+	if (ac != 2)
+	{
+		printf("USAGE: monty file\n);
+		exit(EXIT_FAILURE);
+	}
+
+
+	return (0);
+}
