15b3aeeae55328e48ea0486797691e19297d5491
Juweria86 <jujumelmi143@gmail.com>
2023-10-18 10:57:40 +0300

    creatinf pop function

--- exec_opcode.c
+++ exec_opcode.c
@@ -11,6 +11,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
 				{"push", _push}, {"pall", _pall}, {"pint", _pint},
+				{"pop", _pop},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
--- pint_pop.c
+++ pint_pop.c
@@ -16,3 +16,24 @@ void _pint(stack_t **h, unsigned int n)
 	}
 	printf("%d\n", (*h)->n);
 }
+/**
+ * _pop - removes top element of stack
+ * @h: header
+ * @n: line number
+ */
+void _pop(stack_t **h, unsigned int n)
+{
+	stack_t *temp;
+
+	if (!*h)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", n);
+		fclose(bus.file_ptr);
+		free(bus.content_ptr);
+		free_s(*h);
+		exit(EXIT_FAILURE);
+	}
+	temp = *h;
+	*h = temp->next;
+	free(temp);
+}
