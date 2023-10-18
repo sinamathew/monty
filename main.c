9db171d8a1e8c2598eef7b94f310a8ded35c5cfb
Juweria86 <jujumelmi143@gmail.com>
2023-10-18 07:34:18 +0300

    main file

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+bus_t bus;
+
 /**
  * main - the monty main code
  * @ac: argument counter
@@ -11,19 +13,41 @@
  *
  * By: Sina & Elmi
  */
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	if (ac != 2)
+	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	else
+	file = fopen(argv[1], "r");
+	bus.file_ptr = file;
+	if (!file)
 	{
-		printf("All good\n");
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content_ptr = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_s(stack);
+	fclose(file);
 
 	return (0);
 }
