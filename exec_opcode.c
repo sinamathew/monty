d577db24cf9737f8592fc424cd9ba789c1ac26e4
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-19 00:13:43 +0100

    Hot fix

--- exec_opcode.c
+++ exec_opcode.c
@@ -18,7 +18,6 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"div", _div}, {"mul", _mul},
 				{"mod", _mod}, {"pchar", _pchar},
 				{"pstr", _pstr}, {"rotl", _rotl},
-				{"rotr", _rotr},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
