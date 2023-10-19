4a23a18fe83d19298711b4785aa18c947d9768cd
Juweria86 <jujumelmi143@gmail.com>
2023-10-19 10:50:18 +0300

    rotr

--- monty.h
+++ monty.h
@@ -88,6 +88,7 @@ void _mod(stack_t **h, unsigned int n);
 void _mul(stack_t **h, unsigned int n);
 void _pchar(stack_t **h, unsigned int n);
 void _rotl(stack_t **h, unsigned int n);
+void _rotr(stack_t **h, unsigned int n);
 void unknown_instruction(unsigned int line_no, char *line);
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
 
